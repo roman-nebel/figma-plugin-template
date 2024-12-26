b452b492e0868c377f37becef8b92f2e742cbf10
Roman Nebel <im.roman.nebel@gmail.com>
2024-12-26 14:20:26 +0100

    Expand basic codebase

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,22 @@
-function uiMessageHandler(message) {
-  console.log('Message from UI:', message);
-  // Fetch UI messages
+function sendMessage(type: string, data: any) {
+  figma.ui.postMessage({ type, data });
+}
+
+function sendEchoMessage() {
+  sendMessage('ECHO', { message: 'Hello from plugin' });
+}
+
+function messageHandler({ type, data }) {
+  switch (type) {
+    case 'TEST':
+      console.log(`TEST message: ${data.message}`);
+      sendEchoMessage()
+      break;
+    default:
+      console.log(`Unhandled UI message: ${type}`, data);
+  }
 }
 
 figma.showUI(__html__);
 
-figma.ui.onmessage = uiMessageHandler;
+figma.ui.onmessage = messageHandler;
--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/ui.css';
 import Title from 'antd/es/typography/Title';
+import Button from 'antd/es/button';
+import { Typography } from 'antd';
+const { Text } = Typography;
+
+function sendMessage(type: string, data: any) {
+  parent.postMessage({ pluginMessage: { type, data } }, '*');
+}
+
+function sendTestMessage() {
+  sendMessage('TEST', { message: 'Hello from UI' });
+}
 
 function App() {
-  return <Title level={2}>Start Your Jorney!</Title>;
+  // App state
+  const [data, setData] = useState(null);
+
+  function messageHandler(event: MessageEvent) {
+    const { type, data } = event.data.pluginMessage;
+    switch (type) {
+      case 'ECHO':
+        console.log(`ECHO message: ${data.message}`);
+        setData(data.message);
+        break;
+      default:
+        console.log(`Unhandled plugin message: ${type}`, data);
+    }
+  }
+
+  useEffect(() => {
+    /* Put some logic here */
+
+    // Handle messages from main plugin code
+    window.addEventListener('message', messageHandler);
+  }, []);
+  return (
+    <>  
+      <Title level={2}>Start Your Jorney!</Title>
+      <Button onClick={sendTestMessage}>Test</Button>
+      {data && <Text>{data}</Text>}
+    </>
+  );
 }
 
 export default App;
